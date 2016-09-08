49950b2d7cfb6c89b5e438961b1bf4b0c2fbdf5f
mindy <mindyhammock@gmail.com>
2016-09-08 16:23:31 -0400

    first commit

---
+++ GroupProject/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Lisa is cooking muffins.  The recipe calls for 7 cups of sugar.  She has already put in 2 cups.
+            How many more cups does she need to put in?
+
+            int totalCupsOfSugar = 7;
+            int cupsPutIn = 2;
+            int remainingCups;
+            remainingCups = totalCupsOfSugar - cupsPutIn;
+
+            Console.WriteLine(remainingCups);
+            Console.ReadKey();
+            */
+
+            /*How many packages of diapers can you buy with $40 of one package cost $8?*/
+
+            /*int packagesOfDiapers;
+            int costOfEachPackage = 8;
+            int totalDiaperBudget = 40;
+
+            packagesOfDiapers = totalDiaperBudget / costOfEachPackage;
+
+            Console.WriteLine(packagesOfDiapers);
+            Console.ReadKey();*/
+
+            //Last week Julia ran 30 miles more than Pranav.  Julia ran 47 miles.  How many miles did Pranav run//
+
+            /*int juliaMiles = 47;
+            int PranavMiles;
+            int juliaAdditionalMiles = 30;
+
+            PranavMiles = juliaMiles - juliaAdditionalMiles;
+
+            Console.WriteLine(PranavMiles);
+            Console.ReadKey();*/
+
+            //After paying $5.12 for a salad, Norachai has $27.10.How much money did he have before buying the salad ?
+
+            /*float amountPaidForSalid = 5.12f;
+            float NorachaiChange = 27.10f;
+            float totalBeforeSalid;
+
+            totalBeforeSalid = NorachaiChange + amountPaidForSalid;
+
+            Console.WriteLine(totalBeforeSalid);
+            Console.ReadKey();*/
+
+            //Aliyah had $24 to spend on seven pencils.  After buying them she had $10.  How much did each pencil cost?
+
+            /*int amountToSpendOnPencils = 24;
+            int amountLeftOver = 10;
+            int costOfEachPencil;
+            int totalPencilsBought = 7;
+
+            costOfEachPencil = amountToSpendOnPencils - amountLeftOver;
+            costOfEachPencil = costOfEachPencil / totalPencilsBought;
+
+            Console.WriteLine(costOfEachPencil);
+            Console.ReadKey();*/
+
+           
+
+
+
+
+
+
+
+
+       
+
+
+
+
+
+
+
+
+        }
+    }
+}
